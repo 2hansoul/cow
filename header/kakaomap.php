4f453e36d5b16108dbfb9e252a63068084c73341
hansol <hansol@localhost.localdomain>
2022-03-17 17:02:07 +0900

    c

--- header/kakaomap.php
+++ header/kakaomap.php
@@ -13,14 +13,14 @@
 <script>
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
-        center: new kakao.maps.LatLng(37.52406 , 127.03894), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(37.53987 , 127.17227), // 지도의 중심좌표
+        level: 8 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 // 마커를 표시할 위치입니다
-var position =  new kakao.maps.LatLng(37.52406 , 127.03894);
+var position =  new kakao.maps.LatLng(37.53987 , 127.17227);
 
 // 마커를 생성합니다
 var marker = new kakao.maps.Marker({
