4629ea5e7e0c75a3ec38f92a9d3f041ee3f4a5d5
hansol <hansol@localhost.localdomain>
2022-03-17 16:55:28 +0900

    ㅊ

--- header/harmae.php
+++ header/harmae.php
@@ -490,7 +490,7 @@
         <section class="page-section" id="contact">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Contact Us</h2>
+                    <h2 class="section-heading text-uppercase">오시는길</h2>
                     <h3 class="section-subheading text-muted">Bucket Studio</h3>
                 </div>
                 <!-- * * * * * * * * * * * * * * *-->
