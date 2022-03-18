78447ce786a2532e55964e0a74a0891c4d120585
2hansoul <2hansoul@naver.com>
2022-03-18 11:05:36 +0000

    dd

--- header/harmae.php
+++ header/harmae.php
@@ -17,7 +17,7 @@
         <link href="../css/styles.css" rel="stylesheet" />
 		<!-- background CSS (includes Bootstrap)-->
 		<!-- slide CSS (includes Bootstrap)-->
-        <!--aaaaaaaaaaaaaaaaaaaaaaaaaa-->
+        <!--bbbbbbbbbbbbbbbbbbbb->
     </head>
     <body id="page-top">
         <!-- Navigation-->
